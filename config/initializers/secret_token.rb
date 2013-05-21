05038ad9b804d7fd550ac3e22706924c0326face
Dan Kraus <dskraus@gmail.com>
2013-05-20 19:44:04 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarWarsHolocron::Application.config.secret_token = '1cd0d4f0ff57e30c503572a631bfbddacd7b80493f585fb270545dc17d5089abea640a22b408c73194774a27df71edcf0124da54b8290da64107893d81109ce1'
