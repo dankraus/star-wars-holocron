2fea1be89b06c9eb18ae18b88136eae69eb45fb2
Dan Kraus <dskraus@gmail.com>
2013-05-24 18:14:37 -0500

    Move feed import to Feed model. Update feed_sync:all rake task to call model method instead of passing feed obj to the importer class

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,4 +5,18 @@ class Feed < ActiveRecord::Base
   validates_presence_of :name, :url
 
   has_many :news_entries
+
+  def import_feed
+    parsed_feed = Feedzirra::Feed.fetch_and_parse(self.url)
+
+    parsed_feed.entries.each do |entry|
+      self.news_entries.where(url: entry.url).first_or_create(
+          title: entry.title,
+          summary: entry.summary,
+          content: entry.content,
+          url: entry.url,
+          published: entry.published
+      )
+    end
+  end
 end
